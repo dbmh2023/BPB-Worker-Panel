373b871d9d55b2ce5e9b976ab8eeb341deac9574
dbmh2023 <127428305+dbmh2023@users.noreply.github.com>
2024-07-01 23:10:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '1cc756a7-0f6d-4864-a438-5dfdad33dca8';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
